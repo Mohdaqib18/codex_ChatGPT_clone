240e88bf887b88b0effde8661cd03b55cb4b0d0d
aqib18 <mohammad.aqib@ylytic.com>
2023-02-12 02:01:09 +0530

    finalised the chatGPT functionality

--- client/script.js
+++ client/script.js
@@ -74,11 +74,38 @@ const handleSubmit = async (e) => {
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+
   const messageDiv = document.getElementById(uniqueId);
 
  loader(messageDiv);
 
 
+//fetch data from the server
+
+const response = await fetch("http://localhost:5000", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    prompt: data.get("prompt")
+  })
+
+})
+
+clearInterval(loadInterval);
+messageDiv.innerHTML = "";
+
+if(response.ok){
+  const data = await response.json();
+  const parsedData = data.bot.trim();
+
+  typeText(messageDiv, parsedData)
+}else{
+  const err = await response.text();
+  messageDiv.innerHTML = "Something went wrong";
+  alert(err);
+}
 
 }
 
