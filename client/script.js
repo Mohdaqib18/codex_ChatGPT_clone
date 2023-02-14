6266970f852026327ab614b3b5ad3ca90a217611
aqib18 <mohammad.aqib@ylytic.com>
2023-02-14 09:14:51 +0530

    api key changed

--- client/script.js
+++ client/script.js
@@ -31,88 +31,80 @@ function typeText(element, text) {
 	}, 20);
 }
 
-function generateUniqueId(){
-  const timestamp = Date.now();
+function generateUniqueId() {
+	const timestamp = Date.now();
 
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+	const randomNumber = Math.random();
+	const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`
+	return `id-${timestamp}-${hexadecimalString}`;
 }
 
-function chatStripe(isAi, value, uniqueId){
-  return (
-    `<div class="wrapper ${isAi && 'ai'}">
+function chatStripe(isAi, value, uniqueId) {
+	return `<div class="wrapper ${isAi && "ai"}">
       <div class="chat">
        <div class="profile">
         <img 
-        src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}"
+        src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}"
         />
        </div>
        <div class="message" id=${uniqueId}>${value}</div>
       </div>
     </div>
-    `
-  )
-
+    `;
 }
 
 const handleSubmit = async (e) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  const data = new FormData(form);
+	const data = new FormData(form);
 
-  //users chat stripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+	//users chat stripe
+	chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
-  form.reset();
+	form.reset();
 
-  //bots chatstripe
+	//bots chatstripe
 
-  const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " " , uniqueId);
+	const uniqueId = generateUniqueId();
+	chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+	chatContainer.scrollTop = chatContainer.scrollHeight;
 
+	const messageDiv = document.getElementById(uniqueId);
 
-  const messageDiv = document.getElementById(uniqueId);
+	loader(messageDiv);
 
- loader(messageDiv);
+	//fetch data from the server
 
+	const response = await fetch("http://localhost:5000", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			prompt: data.get("prompt"),
+		}),
+	});
 
-//fetch data from the server
+	clearInterval(loadInterval);
+	messageDiv.innerHTML = "";
 
-const response = await fetch("http://localhost:5000", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    prompt: data.get("prompt")
-  })
+	if (response.ok) {
+		const data = await response.json();
+		const parsedData = data.bot.trim();
 
-})
-
-clearInterval(loadInterval);
-messageDiv.innerHTML = "";
-
-if(response.ok){
-  const data = await response.json();
-  const parsedData = data.bot.trim();
-
-  typeText(messageDiv, parsedData)
-}else{
-  const err = await response.text();
-  messageDiv.innerHTML = "Something went wrong";
-  alert(err);
-}
-
-}
-
+		typeText(messageDiv, parsedData);
+	} else {
+		const err = await response.text();
+		messageDiv.innerHTML = "Something went wrong";
+		alert(err);
+	}
+};
 
 form.addEventListener("submit", handleSubmit);
-form.addEventListener("keyup", (e) =>{
-  if(e.keyCode === 13){
-    handleSubmit(e)
-  }
-})
+form.addEventListener("keyup", (e) => {
+	if (e.keyCode === 13) {
+		handleSubmit(e);
+	}
+});
